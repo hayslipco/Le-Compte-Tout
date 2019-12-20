95ac131197d54f8fc245a3bbedd87e8151584de9
Corwin <o>
2019-12-20 13:13:46 +0100

    adding content-type to header

--- server/index.php
+++ server/index.php
@@ -3,7 +3,8 @@ header("Access-Control-Allow-Origin: *");
 
 header('Access-Control-Allow-Methods: GET, POST');
 
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-Requested-With, content-type, origin");
+header('Content-Type: text/plain');
 
 require('../vendor/autoload.php');
 
