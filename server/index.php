cccec193bb943be40b9b3fbb134f9a5659b7c897
Corwin <o>
2019-12-20 12:59:47 +0100

    Adding headers to index.php

--- server/index.php
+++ server/index.php
@@ -1,3 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 require('../vendor/autoload.php');
+
 ?>
