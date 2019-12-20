54855755ed4743164ffc3ac5ff129761d10a51e9
Corwin <o>
2019-12-20 13:33:14 +0100

    added capitals to "Content-Type"
    big zarma

--- server/index.php
+++ server/index.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 
 header('Access-Control-Allow-Methods: GET, POST');
 
-header("Access-Control-Allow-Headers: X-Requested-With, content-type, origin");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, origin");
 header('Content-Type: text/plain');
 
 require('../vendor/autoload.php');
