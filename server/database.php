5478776ec485f840d0a949c9f9b527fc80e9423f
Corwin <o>
2019-12-20 13:44:51 +0100

    added headers

--- server/database.php
+++ server/database.php
@@ -15,10 +15,12 @@ $user = $url["user"];
 $password = $url["pass"];
 $dbName = substr($url["path"], 1); */
 
-
-
-
 header("Access-Control-Allow-Origin: *");
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, origin");
+header('Content-Type: text/plain');
 
 class database{
     
