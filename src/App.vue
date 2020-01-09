4d874af9d306dec6e5a97da77d40955b85aa7495
Corwin <lol>
2020-01-09 10:38:54 +0100

    Adding multiple list function

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,38 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" class="mx-auto"/>
     <p class="text-white">Le Compte-Tout</p>
-    <Counter/>
+    <!-- @selectedList listener to make variable available-->
+    <Dashboard v-if="onDashboard" @selectedList="goToList"/>
+    <Counter v-else/>
   </div>
 </template>
 
 <script>
 import Counter from "./components/List.vue";
+import Dashboard from "./components/Dashboard.vue";
 import './assets/css/main.css';
 
 export default {
   name: "app",
   components: {
-    Counter
+    Counter,
+    Dashboard
+  },
+
+  data(){
+    return{
+      onDashboard: true,
+      chosenList: -1,
+    }
+  },
+
+  methods: {
+    //get variable from listener (@selectedList="goToList") and assign to chosenList
+    goToList: function(variable) {
+      this.chosenList = variable;
+      alert(this.chosenList);
+      this.onDashboard = false;
+    }
   }
 };
 </script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul class="text-white my-10 text-2xl hover:cursor-wait">
+            <li class="my-2" v-on:click="submit(0)">liste des courses</li>
+            <li class="my-2" v-on:click="submit(1)">partie de scrabble</li>
+            <li class="my-2" v-on:click="submit(2)">SHK</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            //the data that will be passed to App.vue
+            selectedList: -1,
+        }
+    },
+
+    methods: {
+        //send clicked list to App.vue
+        submit: function(index) {
+            this.selectedList = index;
+            this.$emit("selectedList", this.selectedList);
+        }
+    }
+}
+</script>
