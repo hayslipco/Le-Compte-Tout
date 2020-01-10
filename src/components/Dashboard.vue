7baadf2e6543cab738bc60b9987d9599a13d4b0f
Corwin <o>
2020-01-10 13:00:02 +0100

    beautify dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <ul class="text-white my-10 text-2xl hover:cursor-wait">
-            <li class="my-2" v-on:click="submit(0)">liste des courses</li>
-            <li class="my-2" v-on:click="submit(1)">partie de scrabble</li>
-            <li class="my-2" v-on:click="submit(2)">SHK</li>
+        <ul class="text-white my-10 text-2xl w-64 mx-auto">
+            <li class="my-2 border-2 border-white border-solid rounded hover:bg-green-600" v-on:click="submit(0)">Liste des courses</li>
+            <li class="my-2 border-2 border-white border-solid rounded hover:bg-green-600" v-on:click="submit(1)">Partie de scrabble</li>
+            <li class="my-2 border-2 border-white border-solid rounded hover:bg-green-600" v-on:click="submit(2)">SHK</li>
+            <li class="my-2 border-2 border-gray-500 border-dashed rounded hover:bg-green-600 text-gray-500" v-on:click="submit(2)">Nouvelle liste</li>
         </ul>
     </div>
 </template>
+
+<style>
+    li {cursor: pointer;}
+</style>
 
 <script>
 export default {
