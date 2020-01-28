945b44e0578faf286b8800a23980ad39f893b99a
Corwin <lol>
2020-01-28 16:23:56 +0100

    ergo on login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,24 +7,41 @@
             <input id="pwd" v-model="pwd" placeholder="password" type="password"
             class="border-solid border-2 border-gray-800 text-gray-400 rounded bg-gray-800 h-8 self-center my-2 p-1">
             <br>
-            <button v-on:click="submitLogin"
-             class="px-4 hover:bg-green-800 rounded ml-2 text-gray-400 my-2 w-32">Sign in</button>
+            <input id="signInButton" 
+            v-on:click="submitLogin"
+            v-on:keyup.enter="submitLogin"
+             class="px-4 hover:bg-green-800 rounded ml-2 text-gray-400 my-2 w-32 bg-gray-800 border border-black border-solid"
+             type="button"
+             value="Sign in">
              <br>
              <p id="errorMsg" class="text-red-500"></p>
         </div>
     </div>
 </template>
 
+<style>
+    #signInButton{
+        cursor:pointer;
+    }
+</style>
+
 <script>
 import axios from "axios";
 
 export default {
     created(){
-        window.addEventListener('keypress', this.submitLogin);
+        window.addEventListener('keypress', (e) => {
+            if (e.keyCode !== 13){
+                return
+            }
+
+            e.preventDefault()
+            this.submitLogin();
+        });
     },
 
     destroyed(){
-        window.removeEventListener('keypress', this.submitLogin);
+        //window.removeEventListener('keypress', this.submitLogin);
     },
 
     data(){
@@ -59,6 +76,7 @@ export default {
                 this.username = "";
                 this.pwd = "";
                 document.querySelector("#errorMsg").innerHTML = "Bad creds man...";
+                document.querySelector("#username").select();
             }
         }
     }
