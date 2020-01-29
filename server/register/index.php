9c86f46c17d56b1785f10662f7c55648ce52dc1d
Corwin <lol>
2020-01-29 11:19:00 +0100

    Added user registration

---
+++ server/register/index.php
@@ -0,0 +1,69 @@
+<?php
+include_once('../database.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $data = json_decode(file_get_contents('php://input'), true)["data"];
+
+    //create log file
+    $file = "registerlog.txt";
+    $current = file_get_contents($file);
+
+    //get the corresponding user
+    $username = $data['username'];
+    $pwd = $data['pwd'];
+    $registered = false;
+    $userExists = false;
+
+    $current .= "\n username: $username, password: $pwd";
+    file_put_contents($file, $current);
+    
+    //fetch users from db
+    $query = "SELECT * FROM `t_user` WHERE useUsername = :username";
+    $params = array(array("name" => "username", "value" => $username, "type" => PDO::PARAM_STR));
+    $db = new database();
+    $db->prepareExecute($query, $params);
+    $result = $db->fetchData(PDO::FETCH_ASSOC);
+
+    //if the user exists
+    if(!empty($result) && count($result) == 1){
+        $userExists = true;
+        $registered = false;
+
+        $current .= "\n user exists !, variable: usr: $userExists / reg: $registered";
+        file_put_contents($file, $current);
+    } else{
+        //register user
+        $query = "INSERT INTO t_user (useUsername, usePassword) VALUES(:username, :pwd)";
+        $usrnameParam = array("name" => "username", "value" => $username, "type" => PDO::PARAM_STR);
+        $pwdParam = array("name" => "pwd", "value" => $pwd, "type" => PDO::PARAM_STR);
+        $params = array($usrnameParam, $pwdParam);
+        $db->prepareExecute($query, $params);
+        $result = $db->fetchData(PDO::FETCH_ASSOC);
+
+        $responseArray = json_encode($result);
+        $current .= "\n user 'inserted', response: $responseArray";
+        file_put_contents($file, $current);
+
+        $registered = true;
+    }
+
+    
+} else{
+    $response = "noPOST";
+}
+
+if($registered){
+    $response = array("username" => $username, "registered" => 201);
+} elseif($userExists){
+    $response = array("username" => "", "registered" => -1);
+} else{
+    $response = array("username" => "", "registered" => -2);
+}
+
+
+
+echo json_encode($response);
+exit;
+
+?>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,14 +7,31 @@
             <input id="pwd" v-model="pwd" placeholder="password" type="password"
             class="border-solid border-2 border-gray-800 text-gray-400 rounded bg-gray-800 h-8 self-center my-2 p-1">
             <br>
-            <input id="signInButton" 
+            <input id="confirmPwd" placeholder="confirm password" type="password"
+            class="border-solid border-2 border-gray-800 text-gray-400 rounded bg-gray-800 h-8 self-center my-2 p-1"
+            v-if="registering"
+            v-bind="confirmPwd">
+            <br>
+            <!-- Login button -->
+            <input id="loginButton" 
             v-on:click="submitLogin"
-            v-on:keyup.enter="submitLogin"
-             class="px-4 hover:bg-green-800 rounded ml-2 text-gray-400 my-2 w-32 bg-gray-800 border border-black border-solid"
+             class="px-4 hover:bg-green-800 rounded ml-2 text-white my-2 w-32 bg-gray-800 border border-black border-solid"
              type="button"
-             value="Sign in">
+             value="Login"
+             v-if="!registering">
+             <!-- Sign up button -->
+            <input id="signUpButton" 
+            v-on:click="submitRegister"
+             class="px-4 hover:bg-green-800 rounded ml-2 text-white my-2 w-32 bg-gray-800 border border-black border-solid"
+             type="button"
+             value="Sign up"
+             v-else>
              <br>
              <p id="errorMsg" class="text-red-500"></p>
+             <br>
+             <br>
+             <p class="text-white" v-if="!registering">No account but want to count ? <span class="underline text-green-500 cursor-pointer" v-on:click="toggleSignup">Sing up</span></p>
+             <p class="text-white" v-else>Got an account to count ? <span class="underline text-green-500 cursor-pointer" v-on:click="toggleSignup">Login</span></p>
         </div>
     </div>
 </template>
@@ -41,15 +58,25 @@ export default {
     },
 
     destroyed(){
-        //window.removeEventListener('keypress', this.submitLogin);
+        window.removeEventListener('keypress', (e) => {
+            if (e.keyCode !== 13){
+                return
+            }
+
+            e.preventDefault()
+            this.submitLogin();
+        });
     },
 
     data(){
         return{
             username: "",
             pwd: "",
-            authenticated: false,
-        }
+            confirmPwd: "",
+            authenticated: "",
+            registered: "",
+            registering: false,
+            }
     },
 
     methods: {
@@ -62,22 +89,58 @@ export default {
             .then(response => {
                 this.authenticated = response.data;
                 //console.log(this.authenticated);
-                this.verifyResponse();
+                this.verifyLoginResponse();
             })
             .catch(function(error) {
                 alert("login error" + error);
             });
         },
 
-        verifyResponse: function(){
+        submitRegister: function(){
+            this.data = [];
+            axios.post("http://127.0.0.1/server_ct/register/", {
+                data: {username: this.username, pwd: this.pwd},
+                config: {headers: {"Content-Type": "multipart/form-data"}}
+            })
+            .then(response => {
+                this.registered = response.data;
+                console.log(this.registered);
+                this.verifyRegisterResponse();
+            })
+            .catch(function(error) {
+                alert("login error" + error);
+            });
+        },
+
+        verifyLoginResponse: function(){
             if(this.authenticated.idUser >= 0){
                 this.$emit("user", this.authenticated);
-            } else{
+            }else {
                 this.username = "";
                 this.pwd = "";
                 document.querySelector("#errorMsg").innerHTML = "Bad creds man...";
                 document.querySelector("#username").select();
             }
+        },
+
+        //response for registered: -1 = existing username, -2 other problem, 201 = Created
+        verifyRegisterResponse: function(){
+            if(this.registered.registered == 201){
+                alert(this.username + " has been successfully registered, happy counting!");
+                this.toggleSignup();
+                this.username = "";
+                this.pwd = "";
+                document.querySelector("#errorMsg").innerHTML = this.username + "";
+            } else if(this.registered.registered == -1){
+                document.querySelector("#errorMsg").innerHTML = this.username + " already exists ¯\\_(ツ)_/¯";
+                document.querySelector("#username").select();
+            } else{
+                document.querySelector("#errorMsg").innerHTML = "yeah something went wrong, please forgive.";
+            }
+        },
+
+        toggleSignup: function(){
+            this.registering = !this.registering;
         }
     }
 }
